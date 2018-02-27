813953b6e0f65028791e2d9cd7f121b111f43a60
shyam <bkshyamprasath@gmail.com>
2018-02-27 11:06:54 +0530

    node to connect to both prod and dev db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,16 @@
 var mongoose = require('mongoose') ;
 
 mongoose.Promise = global.Promise ;
-mongoose.connect('mongodb://nodeapidb:password@ds249818.mlab.com:49818/node_todo_api');
+
+if(process.env.NODE_ENV === 'production')
+{
+    mongoose.connect('mongodb://nodeapidb:password@ds249818.mlab.com:49818/node_todo_api');
+    return;
+}
+else {
+  mongoose.connect('mongodb://localhost:27017/TodoApp');
+  return ; 
+}
 
 //mongodb://localhost:27017/TodoApp
 // module.exports = {
